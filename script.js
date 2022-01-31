f799b551ddcf66b47cc01b625fbad735080db21c
Elizabeth <elzbthyorke@gmail.com>
2022-01-31 08:41:53 -0600

    January 31st, 2022, 8:41 am; completed Studio except for the bonus

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 window.addEventListener("load", function(){
     this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
-            for (i=0; i<json.length; i++){
+            for (let i=0; i<json.length; i++){
                 const div = document.getElementById("container");
-                div.innerHTML = `
+                // if (json[i].hoursInSpace >500){
+                // let hoursInSpaceArray = 
+                // console.log(Math.max(json.hoursInSpace))
+                div.innerHTML += `
                    <div class="astronaut">
                         <div class="bio">
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
@@ -18,6 +21,11 @@ window.addEventListener("load", function(){
                         <img class="avatar" src=${json[i].picture}>
                     </div>
                 `;
+            // }
+
+                // console.log("on the loop");
+
+                
             };
         });
     });
