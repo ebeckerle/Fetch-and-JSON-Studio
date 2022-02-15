0c114be83f08250299d3534eb7a631400dfa1c48
Elizabeth <elzbthyorke@gmail.com>
2022-02-14 20:30:54 -0600

    Feb 14 2022, 8:30pm

--- script.js
+++ script.js
@@ -8,24 +8,27 @@ window.addEventListener("load", function(){
                 // if (json[i].hoursInSpace >500){
                 // let hoursInSpaceArray = 
                 // console.log(Math.max(json.hoursInSpace))
+                
                 div.innerHTML += `
                    <div class="astronaut">
                         <div class="bio">
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in Space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                <li id="active-${i +1}">Active: ${json[i].active}</li>
                                 <li>Skills: ${json[i].skills}</li>
                             </ul>
                         </div>
                         <img class="avatar" src=${json[i].picture}>
                     </div>
                 `;
+                if (json[i].active){
+                    console.log(json[i].active);
+                    document.getElementById(`active-${i +1}`).style.color = "green";
+
+                }
+                // document.getElementsByTagName("li").style.color = "green";
             // }
-
-                // console.log("on the loop");
-
-                
             };
         });
     });
